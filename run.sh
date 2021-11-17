cff12a8d085c243681bb8a69f85dd396847ca715
Yongho Kim <ironyonghokim@gmail.com>
2021-11-16 23:05:41 -0600

    Bug fixed in run.sh and yaml

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 cd /tmp
 git clone git@github.com:waggle-sensor/hanwha_camera_client.git
 cd hanwha_camera_client
-python3 setup.py install
+python3 setup.py bdist_wheel
 pip3 install dist/*.whl
 cd /app
-exit python3 -u camera_provisioner.py
+python3 -u camera_provisioner.py
+exit $?
